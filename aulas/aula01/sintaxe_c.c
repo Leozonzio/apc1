6f713703d524efe569c35ac1263743b1bc338539
leozonzio <14638513-leozonzio@users.noreply.replit.com>
2024-08-15 00:48:25 +0000

    codigos da aula 1

---
+++ aulas/aula01/sintaxe_c.c
@@ -0,0 +1,35 @@
+// ISSO é diferente de isso
+//#include
+//main() {
+ // linha_de_codigo;
+//}
+
+#include <stdio.h> //inclusao de bibliotecas
+
+int main() { // funcao principal
+
+  // 1. entrada
+  // levantar meus ingredientes
+  // char, int, float
+  // variaveis
+  // leitura
+  
+  int primeiro_numero;
+  int segundo_numero;
+  int soma;
+  
+  primeiro_numero = 5;
+  segundo_numero = 6;
+  
+  // 2. processamento
+  // calculo: soma, subtração, multiplicação, divisão
+
+  soma = primeiro_numero + segundo_numero;
+  
+  // 3. saida
+  // escrita
+
+  printf("A soma de %i com %i é %i\n", primeiro_numero, segundo_numero, soma);
+  
+  return 0; // retorna o fim do programa
+}
