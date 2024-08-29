b37df46863c91b69862a6c6efee9a053350265fa
Leozonzio <98622940+Leozonzio@users.noreply.github.com>
2024-08-29 00:32:32 +0000

    Adicionando arquivos da pratica03

---
+++ praticas/pratica03/declara_aluno.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main () {
+
+  long int matricula = 00000000;
+  int idade = 99;
+  float altura = 9.90f;
+  float peso = 999.0;
+  char sexo = 'M';
+  
+
+  return 0;
+}
---
+++ praticas/pratica03/declara_livro.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+  long int ISBN = 00000000000;
+  int num_pag = 000;
+  float preco = 0000.00;
+  long int ano = 0000;
+  
+  
+  return 0;
+}
